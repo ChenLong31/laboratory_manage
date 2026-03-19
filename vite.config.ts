95809eed8143a577082fea078a8dd57a83194b49
chenlong2 <19962032644@189.cn>
2026-03-19 18:52:47 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
@@ -22,6 +21,26 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  build: {
+    // 禁用 CSS 代码分割，将所有 CSS 合并成一个文件
+    cssCodeSplit: false,
+
+    rollupOptions: {
+      output: {
+        // 将所有 JS 合并成一个文件
+        manualChunks: undefined,
+        // 设置 JS 输出文件名
+        entryFileNames: "assets/all.js",
+        // 设置 CSS 和其他资源文件输出文件名
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "assets/all.css";
+          }
+          return "assets/[name].[hash].[ext]";
+        }
+      }
     }
   }
 });
